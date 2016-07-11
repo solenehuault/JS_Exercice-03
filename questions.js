c70695cd74c75a5c77a84271666d0628d05f8a7a
solenehuault <solene.huault@gmail.com>
2016-07-11 14:23:53 +0200

    spec 2 done

--- questions.js
+++ questions.js
@@ -2,3 +2,8 @@ function CreationTableauLangages() {
 	langages = ["Html","CSS","Java","PHP"]	
 	return langages;
 };
+
+function CreationTableauNombres() {
+	nombres = [0, 1, 2, 3, 4, 5];
+	return nombres;
+};
